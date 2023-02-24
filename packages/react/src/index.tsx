bbc4435c111cb86f415a37734b2e6f47c8de84ca
Yechao Li <liyechao@yechao.xyz>
2023-02-24 22:39:16 +0800

    style: remove unnecessary comment

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,4 @@
 import ImagePreview from './components/image-preview';
 import LoadingImage from './components/loading-image';
 
-// eslint-disable-next-line import/prefer-default-export
 export { ImagePreview, LoadingImage };
