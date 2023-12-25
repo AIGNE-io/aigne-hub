2aad11c1cea57fb2630adfe55092dc359d5a9014
Yechao Li <liyechao@yechao.xyz>
2023-12-25 14:50:43 +0800

    support trialing

--- blocklets/ai-kit/api/src/libs/payment.ts
+++ blocklets/ai-kit/api/src/libs/payment.ts
@@ -4,7 +4,9 @@ import { Config } from './env';
 
 export async function getActiveSubscriptionOfApp({ appId }: { appId: string }) {
   const subscription = (await payment.subscriptions.list({ 'metadata.appId': appId })).list.find(
-    (i) => i.status === 'active' && i.items.some((j) => j.price.product.id === Config.pricing?.subscriptionProductId)
+    (i) =>
+      ['active', 'trialing'].includes(i.status) &&
+      i.items.some((j) => j.price.product.id === Config.pricing?.subscriptionProductId)
   );
 
   return subscription;
