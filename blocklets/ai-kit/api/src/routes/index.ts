9f86378655aad439f95958b6e338d8c3da8194b3
Yechao Li <liyechao@yechao.xyz>
2024-01-03 15:42:46 +0800

    fix: avoid recursive self-calling

--- blocklets/ai-kit/api/src/routes/index.ts
+++ blocklets/ai-kit/api/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { proxyToAIKit } from '@blocklet/ai-kit/api/call';
 import AIKitConfig from '@blocklet/ai-kit/api/config';
+import { AI_KIT_BASE_URL } from '@blocklet/ai-kit/constants';
 import { Router } from 'express';
+import { parseURL } from 'ufo';
 
 import app from './app';
 import payment from './payment';
@@ -8,8 +10,14 @@ import v1 from './v1';
 
 const router = Router();
 
+const aiKitBaseUrlHostname = parseURL(AI_KIT_BASE_URL).host;
+
 router.use('/v1', (req, res, next) => {
-  if (AIKitConfig.useAIKitService) {
+  if (
+    AIKitConfig.useAIKitService &&
+    // NOTE: avoid recursive self-calling
+    req.hostname !== aiKitBaseUrlHostname
+  ) {
     proxyToAIKit(req.originalUrl as any, { useAIKitService: true })(req, res, next);
   } else {
     v1(req, res, next);
