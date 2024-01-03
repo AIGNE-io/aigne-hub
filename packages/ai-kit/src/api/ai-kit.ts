df5df2c9ef113ffc239b8fc7ea5c08caee29dde8
Yechao Li <liyechao@yechao.xyz>
2024-01-03 20:20:57 +0800

    revert ai-kit api helpers

---
+++ packages/ai-kit/src/api/ai-kit.ts
@@ -0,0 +1,105 @@
+import { AxiosInstance } from 'axios';
+import { ChatCompletionMessageParam } from 'openai/resources';
+
+export const createStatusApi =
+  ({ axios, path }: { axios: AxiosInstance; path: string }): (() => Promise<{ available: boolean }>) =>
+  () =>
+    axios.get(path).then((res) => res.data);
+
+export interface TextCompletions {
+  text?: string;
+}
+
+export interface TextCompletionOptions {
+  model?: string;
+  temperature?: number;
+}
+
+export interface TextCompletionFn<P extends {}> {
+  (options: { stream: true } & TextCompletionOptions & P): Promise<ReadableStream<Uint8Array>>;
+  (options: { stream?: boolean } & TextCompletionOptions & P): Promise<TextCompletions>;
+  (options: { stream?: boolean } & TextCompletionOptions & P): Promise<TextCompletions | ReadableStream<Uint8Array>>;
+}
+
+export const createTextCompletionApi =
+  <P extends {} = { prompt: string } | { messages: ChatCompletionMessageParam[] }>({
+    axios,
+    path,
+    timeout,
+  }: {
+    axios: AxiosInstance;
+    path: string;
+    timeout?: number;
+  }): TextCompletionFn<P> =>
+  async (options) => {
+    const promise: Promise<TextCompletions | ReadableStream<Uint8Array>> = options.stream
+      ? fetch(axios.getUri({ url: path }), {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(options),
+        }).then(async (res) => {
+          if (res.status !== 200) {
+            const text = await res.text();
+            let json: any;
+            try {
+              json = JSON.parse(text);
+            } catch {
+              // eslint-disable-next-line no-empty
+            }
+            throw new Error(json?.error?.message || json?.message || text || res.status);
+          }
+          return res.body!;
+        })
+      : axios
+          .post(path, options)
+          .then(({ data }) => ({ text: data.text ?? data.choices?.[0]?.text ?? data.choices?.[0]?.message?.content }))
+          .catch(processResponseError);
+
+    if (!timeout) {
+      return promise as any;
+    }
+
+    return Promise.race([
+      promise,
+      new Promise((_, reject) => {
+        setTimeout(() => reject(new Error('Timeout')), timeout);
+      }),
+    ]);
+  };
+
+export interface ImageGenerations<T extends { url: string } | { b64_json: string }> {
+  data: T[];
+}
+
+export type ImageGenerationSize = '256x256' | '512x512' | '1024x1024';
+
+export interface ImageGenerationFn<P extends {}> {
+  (options: P & { response_format?: 'url' }): Promise<ImageGenerations<{ url: string }>>;
+  (options: P & { response_format?: 'b64_json' }): Promise<ImageGenerations<{ b64_json: string }>>;
+  (options: P & { response_format?: 'url' | 'b64_json' }): Promise<
+    ImageGenerations<{ url: string } | { b64_json: string }>
+  >;
+}
+
+export const createImageGenerationApi =
+  <P extends {} = { prompt: string; size: ImageGenerationSize; n: number }>({
+    axios,
+    path,
+  }: {
+    axios: AxiosInstance;
+    path: string;
+  }): ImageGenerationFn<P> =>
+  async (options) => {
+    return axios
+      .post(path, options)
+      .then((res) => res.data)
+      .catch(processResponseError);
+  };
+
+function processResponseError(error: any): never {
+  const msg = error.response?.data?.error?.message || error.response?.data?.message;
+  if (msg) {
+    throw new Error(msg);
+  }
+  throw error;
+}
