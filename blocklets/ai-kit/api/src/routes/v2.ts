13b9630eee399a655d8fda68a49ab8ebfeafba3e
leermao <447552708@qq.com>
2025-07-16 08:26:04 +0800

    chore: code review

--- blocklets/ai-kit/api/src/routes/v2.ts
+++ blocklets/ai-kit/api/src/routes/v2.ts
@@ -9,19 +9,21 @@ import { getModel } from '../providers/models';
 
 const router = Router();
 
-const OBSERVABILITY_BLOCKLET_NAME = 'z2qa2GCqPJkufzqF98D8o7PWHrRRSHpYkNhEh';
+const OBSERVABILITY_BLOCKLET_DID = 'z2qa2GCqPJkufzqF98D8o7PWHrRRSHpYkNhEh';
+const AIGNE_HUB_BLOCKLET_DID = 'z8ia3xzq2tMq8CRHfaXj1BTYJyYnEcHbqP8cJ';
+
 AIGNEObserver.setExportFn(async (spans) => {
-  if (!getComponentMountPoint(OBSERVABILITY_BLOCKLET_NAME)) {
+  if (!getComponentMountPoint(OBSERVABILITY_BLOCKLET_DID)) {
     logger.warn('Please install the Observability blocklet to enable tracing agents');
     return;
   }
 
   await call({
-    name: OBSERVABILITY_BLOCKLET_NAME,
+    name: OBSERVABILITY_BLOCKLET_DID,
     method: 'POST',
     path: '/api/trace/tree',
     data: spans.map((x) => {
-      return { ...x, componentId: 'z8ia3xzq2tMq8CRHfaXj1BTYJyYnEcHbqP8cJ' };
+      return { ...x, componentId: AIGNE_HUB_BLOCKLET_DID };
     }),
   }).catch((err) => {
     logger.error('Failed to send trace tree to Observability blocklet', err);
@@ -31,7 +33,6 @@ AIGNEObserver.setExportFn(async (spans) => {
 router.post('/chat', async (req, res) => {
   const model = getModel(req.body, {
     modelOptions: req.body?.options?.modelOptions,
-    clientOptions: req.body?.options?.clientOptions,
   });
   const engine = new AIGNE({ model });
   const aigneServer = new AIGNEHTTPServer(engine);
