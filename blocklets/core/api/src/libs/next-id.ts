8577e0c8e1b688e19c5f513d079fa67e806fc92b
leermao <447552708@qq.com>
2025-09-15 17:07:55 +0800

    fix: bump version

--- blocklets/core/api/src/libs/next-id.ts
+++ blocklets/core/api/src/libs/next-id.ts
@@ -2,10 +2,8 @@ import cluster from 'cluster';
 
 import { Worker } from 'snowflake-uuid';
 
-const nextId = () => {
-  const workerId = cluster.isWorker ? cluster.worker?.id || 0 : 0;
-  const idGenerator = new Worker(workerId);
-  return idGenerator.nextId().toString();
-};
+const workerId = cluster.isWorker ? cluster.worker?.id || 0 : 0;
+const idGenerator = new Worker(workerId);
+const nextId = () => idGenerator.nextId().toString();
 
 export default nextId;
