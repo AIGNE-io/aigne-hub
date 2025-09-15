1aa61145d11468a20743ee3c0706afc4058c7c0a
leermao <447552708@qq.com>
2025-09-15 17:16:12 +0800

    chore: code review

--- blocklets/core/api/src/libs/next-id.ts
+++ blocklets/core/api/src/libs/next-id.ts
@@ -2,7 +2,7 @@ import cluster from 'cluster';
 
 import { Worker } from 'snowflake-uuid';
 
-const workerId = cluster.isWorker ? cluster.worker?.id || 0 : 0;
+const workerId = cluster.isWorker ? (cluster.worker?.id || 0) % 1024 : 0;
 const idGenerator = new Worker(workerId);
 const nextId = () => idGenerator.nextId().toString();
 
